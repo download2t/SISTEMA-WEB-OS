d654a91d1a2a053aec59f6dfc21457bea347ab74
Matheus Duarte Barros <mtduarte.b@gmail.com>
2025-01-15 08:17:48 -0300

    removido pasta media no git ignore

--- sistema_geral/banco_senhas/admin.py
+++ sistema_geral/banco_senhas/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
+from django.db.models import Q
 from .models import Categoria, Senha
+from django.contrib.auth.models import User  # Importando User, caso esteja usando o modelo padrão
+
+# Filtro personalizado para o usuário
+class UsuarioFilter(admin.SimpleListFilter):
+    title = 'usuário'  # O título do filtro
+    parameter_name = 'usuario'  # O nome do parâmetro na URL
+
+    def lookups(self, request, model_admin):
+        # Retorna os usuários para que possam ser selecionados no filtro
+        users = set(Categoria.objects.values_list('usuario', flat=True))
+        # Exibir o nome do usuário 
+        return [(user.id, str(user)) for user in User.objects.filter(id__in=users)]  # Exibindo nome completo
+
+    def queryset(self, request, queryset):
+        # Filtra as categorias que pertencem ao usuário selecionado
+        if self.value():
+            return queryset.filter(usuario_id=self.value())  # Filtra com o id do usuário
+        return queryset
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'is_coletiva', 'usuario')
-    list_filter = ('is_coletiva',)
+    list_filter = ('is_coletiva', UsuarioFilter)  # Adicionando o filtro personalizado
 
 @admin.register(Senha)
 class SenhaAdmin(admin.ModelAdmin):
-    list_display = ('descricao', 'is_coletiva', 'usuario', 'categoria')
+    list_display = ('descricao','user','senha', 'is_coletiva', 'usuario', 'categoria','link')
     list_filter = ('is_coletiva', 'categoria')
